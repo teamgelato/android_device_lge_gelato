65986515b61292fde5948e3e751f2e4d5c9e8904
Ricardo Gomez <ricardo.gomez331@gmail.com>
2013-09-01 21:41:42 -0500

    fix-up cm.mk for cm9
    Change-Id: I4a8777bf018fa9bd11224d80c5797339f660ab5d

--- cm.mk
+++ cm.mk
@@ -11,31 +11,14 @@
 $(call inherit-product, device/lge/gelato/full_gelato.mk)
 
 # Inherit some common cyanogenmod stuff.
-$(call inherit-product, vendor/cyanogen/products/common.mk)
+$(call inherit-product, vendor/cm/config/common.mk)
 
 # Include FM-Radio stuff (does not work at this moment)
-$(call inherit-product, vendor/cyanogen/products/bcm_fm_radio.mk)
+$(call inherit-product, vendor/cm/config/bcm_fm_radio.mk)
 
 PRODUCT_BRAND := LGE
 PRODUCT_DEVICE := gelato
-PRODUCT_MODEL := LG-$(SUB_MODEL)
+PRODUCT_MODEL := LG-VM701
 PRODUCT_MANUFACTURER := LGE
-PRODUCT_NAME := cyanogen_gelato_$(SUB_MODEL)
+PRODUCT_NAME := cm_gelato
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=gelato BUILD_UTC_DATE=0 BUILD_ID=GWK74 BUILD_DISPLAY_ID="GWK74" BUILD_FINGERPRINT=lge/lge_gelato/VM701:2.3.4/GRJ22/ZV4.19cd75186d:user/release-keys PRIVATE_BUILD_DESC="lge_gelato-user 2.3.4 GRJ22 ZV4.19cd75186d release-keys"
-
-# Live wallpaper packages
-PRODUCT_PACKAGES += \
-        LiveWallpapersPicker \
-
-ifdef CYANOGEN_NIGHTLY
-        CMVERSION := CyanogenMod-7.2.0-$(shell date +%Y%m%d)-NIGHTLY
-else
-    ifdef CYANOGEN_RELEASE
-        CMVERSION := CyanogenMod-7.2.0
-    else
-        CMVERSION := CyanogenMod-7.2.0-$(PRODUCT_MODEL)-teamgelato-alpha-$(shell date +%Y%m%d)
-    endif
-endif
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.modversion=$(CMVERSION)
